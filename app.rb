7e225d9ac4bd783b54bd5716f9abf005f802995c
Rose Ames <rose@happyspork.com>
2014-05-31 21:28:24 -0400

    improve examples in app.rb

--- app.rb
+++ app.rb
@@ -1,17 +1,31 @@
 require './server.rb' 
 require './router.rb' 
 
+router = Router.new
+
+
+# example with no captured variables from the route
 
 route1 = lambda do |captures|
-  return "<html><head><title>Bleep Bloop</title></head><body>booooooork</body></html>"
+  return "<html><head><title>Bleep Bloop</title></head><body>Sample dynamically constructed page</body></html>"
 end
+
+router.add_route(/\/bleep/, route1)
+
+
+# constructing a page using named captures from the route url
 
 route2 = lambda do |captures|
-  return "hello " + captures["person"] + "!"
+  name = captures["person"]
+  if name == ""
+    name = "there"
+  end
+  return "<html><head><title>Hello " + name + "!</title></head><body><h1>Sample capture page</h1><br>This is a <b>FREE</b> personalized greeting for " + name + "!  Hi " + name + "!</body></html>"
 end
 
-router = Router.new
-router.add_route(/\/bleepbloop/, route1)
 router.add_route(/\/greet\/(?<person>[^\/]*)\/?/, route2)
+
+
+# creating a server with the above routes, and running it on port 9393
 
 Server.new({"port" => 9393, "router" => router}).start
